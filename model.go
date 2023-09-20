152b012bcddc47f6cf3a93c8d05736ab057ea219
McGyv246 <122812734+McGyv246@users.noreply.github.com>
2023-09-20 14:34:48 +0200

    Added list with namespaces

--- model.go
+++ model.go
@@ -1,8 +1,18 @@
 package main
 
 import (
+	"fmt"
+	"github.com/charmbracelet/bubbles/list"
 	"github.com/charmbracelet/bubbles/spinner"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+	"io"
+	"strings"
+)
+
+var (
+	itemStyle         = lipgloss.NewStyle().PaddingLeft(4)
+	selectedItemStyle = lipgloss.NewStyle().PaddingLeft(2).Foreground(lipgloss.Color("170"))
 )
 
 type K8mpassModel struct {
@@ -11,15 +21,55 @@ type K8mpassModel struct {
 	clusterConnectionSpinner spinner.Model
 	isConnected              bool
 	command                  NamespaceOperation
+	list                     list.Model
+	listItems                []list.Item
 }
+
+type item struct {
+	name string
+}
+
+func (i item) Title() string {
+	return i.name
+}
+func (i item) FilterValue() string {
+	return i.name
+}
+
+type itemDelegate struct{}
+
+func (d itemDelegate) Height() int                             { return 1 }
+func (d itemDelegate) Spacing() int                            { return 0 }
+func (d itemDelegate) Update(_ tea.Msg, _ *list.Model) tea.Cmd { return nil }
+func (d itemDelegate) Render(w io.Writer, m list.Model, index int, listItem list.Item) {
+	mItem, ok := listItem.(item)
+	if !ok {
+		return
+	}
+
+	str := fmt.Sprintf("%d. %s", index+1, mItem.name)
+
+	fn := itemStyle.Render
+	if index == m.Index() {
+		fn = func(s ...string) string {
+			return selectedItemStyle.Render("> " + strings.Join(s, " "))
+		}
+	}
+
+	fmt.Fprint(w, fn(str))
+}
+
+var docStyle = lipgloss.NewStyle().Margin(1, 2)
 
 func initialModel() K8mpassModel {
 	s := spinner.New()
 	s.Spinner = spinner.Line
-	return K8mpassModel{
+	model := K8mpassModel{
 		clusterConnectionSpinner: s,
 		command:                  GetAllNamespacesOperation,
+		list:                     list.New([]list.Item{}, itemDelegate{}, 80, 15),
 	}
+	return model
 }
 
 func (m K8mpassModel) Init() tea.Cmd {
@@ -36,20 +86,33 @@ func (m K8mpassModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		switch msg.String() {
 		case "ctrl+c", "esc":
 			return m, tea.Quit
+		case "enter":
 		}
+
+	case tea.WindowSizeMsg:
+		h, v := docStyle.GetFrameSize()
+		m.list.SetSize(msg.Width-h, msg.Height-v)
+
 	case clusterConnectedMsg:
 		m.isConnected = true
 		m.cluster.kubernetes = msg.clientset
-		command := m.command.Command(m, "review-galileo-cgmgpharm-40833-fe")
+		command := m.command.Command(m, "")
 		cmds = append(cmds, command)
 	case namespacesNamesMsg:
-		//TODO generate list -> msg.body
-		command := m.command.Command(m, "review-galileo-cgmgpharm-40833-fe")
-		cmds = append(cmds, command)
+		namespacesList := []list.Item{}
+		for _, val := range msg.body {
+			namespacesList = append(namespacesList, item{val})
+		}
+		m.list = list.New(namespacesList, itemDelegate{}, 80, 15)
+		m.list.Title = "Select the namespace"
 	}
 	if !m.isConnected {
 		s, cmd := m.clusterConnectionSpinner.Update(msg)
 		m.clusterConnectionSpinner = s
+		cmds = append(cmds, cmd)
+	} else {
+		var cmd tea.Cmd
+		m.list, cmd = m.list.Update(msg)
 		cmds = append(cmds, cmd)
 	}
 	return m, tea.Batch(cmds...)
@@ -61,7 +124,8 @@ func (m K8mpassModel) View() string {
 		s += m.clusterConnectionSpinner.View()
 		s += "Connecting to the cluster..."
 	} else {
-		s += "Connection successful! Press esc to quit"
+		return docStyle.Render(m.list.View())
+		// s += "Connection successful! Press esc to quit"
 	}
 	s += "\n"
 	return s
