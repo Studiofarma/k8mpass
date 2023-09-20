ef953486ce11ef2b1b13e15945cfee4effacafdd
Ivan Lidonni <ivan.lidonni@cgm.com>
2023-09-20 11:49:47 +0200

    list try

--- model.go
+++ model.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	"github.com/charmbracelet/bubbles/list"
 	"github.com/charmbracelet/bubbles/spinner"
+
+	"github.com/charmbracelet/bubbles/key"
+	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 )
@@ -20,6 +22,45 @@ var (
 				Render
 )
 
+type listKeyMap struct {
+	toggleSpinner    key.Binding
+	toggleTitleBar   key.Binding
+	toggleStatusBar  key.Binding
+	togglePagination key.Binding
+	toggleHelpMenu   key.Binding
+	insertItem       key.Binding
+}
+
+func newListKeyMap() *listKeyMap {
+	return &listKeyMap{
+		toggleSpinner: key.NewBinding(
+			key.WithKeys("s"),
+			key.WithHelp("s", "toggle spinner"),
+		),
+		toggleTitleBar: key.NewBinding(
+			key.WithKeys("T"),
+			key.WithHelp("T", "toggle title"),
+		),
+		toggleStatusBar: key.NewBinding(
+			key.WithKeys("S"),
+			key.WithHelp("S", "toggle status"),
+		),
+		togglePagination: key.NewBinding(
+			key.WithKeys("P"),
+			key.WithHelp("P", "toggle pagination"),
+		),
+		toggleHelpMenu: key.NewBinding(
+			key.WithKeys("H"),
+			key.WithHelp("H", "toggle help"),
+		),
+	}
+}
+
+type delegateKeyMap struct {
+	choose key.Binding
+	remove key.Binding
+}
+
 type K8mpassModel struct {
 	error                    errMsg
 	cluster                  kubernetesCluster
@@ -27,18 +68,74 @@ type K8mpassModel struct {
 	isConnected              bool
 	command                  NamespaceOperation
 	namespaces               list.Model
+	keys                     *listKeyMap
+	delegateKeys             *delegateKeyMap
 }
 
 type item struct {
 	title, desc string
 }
 
-func (i item) FilterValue() string {
-	//TODO implement me
-	panic("implement me")
+func (i item) Title() string       { return i.title }
+func (i item) Description() string { return i.desc }
+func (i item) FilterValue() string { return i.title }
+func newDelegateKeyMap() *delegateKeyMap {
+	return &delegateKeyMap{
+		choose: key.NewBinding(
+			key.WithKeys("enter"),
+			key.WithHelp("enter", "choose"),
+		),
+	}
 }
 
+func newItemDelegate(keys *delegateKeyMap) list.DefaultDelegate {
+	d := list.NewDefaultDelegate()
+
+	d.UpdateFunc = func(msg tea.Msg, m *list.Model) tea.Cmd {
+		var title string
+
+		if i, ok := m.SelectedItem().(item); ok {
+			title = i.Title()
+		} else {
+			return nil
+		}
+
+		switch msg := msg.(type) {
+		case tea.KeyMsg:
+			switch {
+			case key.Matches(msg, keys.choose):
+				return m.NewStatusMessage(statusMessageStyle("You chose " + title))
+
+			case key.Matches(msg, keys.remove):
+				index := m.Index()
+				m.RemoveItem(index)
+				if len(m.Items()) == 0 {
+					keys.remove.SetEnabled(false)
+				}
+				return m.NewStatusMessage(statusMessageStyle("Deleted " + title))
+			}
+		}
+
+		return nil
+	}
+
+	help := []key.Binding{keys.choose, keys.remove}
+
+	d.ShortHelpFunc = func() []key.Binding {
+		return help
+	}
+
+	d.FullHelpFunc = func() [][]key.Binding {
+		return [][]key.Binding{help}
+	}
+
+	return d
+}
 func initialModel() K8mpassModel {
+	var (
+		delegateKeys = newDelegateKeyMap()
+		listKeys     = newListKeyMap()
+	)
 	s := spinner.New()
 	s.Spinner = spinner.Line
 
@@ -68,19 +165,33 @@ func initialModel() K8mpassModel {
 		item{title: "Terrycloth", desc: "In other words, towel fabric"},
 	}
 
-	groceryList := list.New(items, list.NewDefaultDelegate(), 0, 0)
+	delegate := newItemDelegate(newDelegateKeyMap())
+	groceryList := list.New(items, delegate, 0, 0)
 	groceryList.Title = "Stocazzo"
 	groceryList.Styles.Title = titleStyle
+	groceryList.AdditionalFullHelpKeys = func() []key.Binding {
+		return []key.Binding{
+			listKeys.toggleSpinner,
+			listKeys.insertItem,
+			listKeys.toggleTitleBar,
+			listKeys.toggleStatusBar,
+			listKeys.togglePagination,
+			listKeys.toggleHelpMenu,
+		}
+	}
 
 	return K8mpassModel{
 		clusterConnectionSpinner: s,
 		command:                  WakeUpReviewOperation,
 		namespaces:               groceryList,
+		keys:                     listKeys,
+		delegateKeys:             delegateKeys,
 	}
 }
 
 func (m K8mpassModel) Init() tea.Cmd {
-	return tea.Batch(m.clusterConnectionSpinner.Tick, clusterConnect)
+	return tea.EnterAltScreen
+	//return tea.Batch(m.clusterConnectionSpinner.Tick, clusterConnect)
 }
 
 func (m K8mpassModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
@@ -94,22 +205,63 @@ func (m K8mpassModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		case "ctrl+c", "esc":
 			return m, tea.Quit
 		}
+		if m.namespaces.FilterState() == list.Filtering {
+			break
+		}
+
+		switch {
+		case key.Matches(msg, m.keys.toggleSpinner):
+			cmd := m.namespaces.ToggleSpinner()
+			return m, cmd
+
+		case key.Matches(msg, m.keys.toggleTitleBar):
+			v := !m.namespaces.ShowTitle()
+			m.namespaces.SetShowTitle(v)
+			m.namespaces.SetShowFilter(v)
+			m.namespaces.SetFilteringEnabled(v)
+			return m, nil
+
+		case key.Matches(msg, m.keys.toggleStatusBar):
+			m.namespaces.SetShowStatusBar(!m.namespaces.ShowStatusBar())
+			return m, nil
+
+		case key.Matches(msg, m.keys.togglePagination):
+			m.namespaces.SetShowPagination(!m.namespaces.ShowPagination())
+			return m, nil
+
+		case key.Matches(msg, m.keys.toggleHelpMenu):
+			m.namespaces.SetShowHelp(!m.namespaces.ShowHelp())
+			return m, nil
+
+			//case key.Matches(msg, m.keys.insertItem):
+			//	m.delegateKeys.remove.SetEnabled(true)
+			//	newItem := m..next()
+			//	insCmd := m.namespaces.InsertItem(0, newItem)
+			//	statusCmd := m.namespaces.NewStatusMessage(statusMessageStyle("Added " + newItem.Title()))
+			//	return m, tea.Batch(insCmd, statusCmd)
+			//
+		}
 	case clusterConnectedMsg:
 		m.isConnected = true
 		m.cluster.kubernetes = msg.clientset
 		command := m.command.Command(m, "review-hack-cgmgpharm-47203-be")
 		cmds = append(cmds, command)
 	}
+
 	if !m.isConnected {
 		s, cmd := m.clusterConnectionSpinner.Update(msg)
 		m.clusterConnectionSpinner = s
 		cmds = append(cmds, cmd)
 	}
+
+	newListModel, cmd := m.namespaces.Update(msg)
+	m.namespaces = newListModel
+	cmds = append(cmds, cmd)
 	return m, tea.Batch(cmds...)
 }
 
 func (m K8mpassModel) View() string {
-	s := ""
+	/*s := ""
 	if !m.isConnected {
 		s += m.clusterConnectionSpinner.View()
 		s += "Connecting to the cluster..."
@@ -117,5 +269,7 @@ func (m K8mpassModel) View() string {
 		s += "Connection successful! Press esc to quit"
 	}
 	s += "\n"
-	return s
+	s += appStyle.Render(m.namespaces.View())*/
+	return appStyle.Render(m.namespaces.View())
+	//return s
 }
