69d6fca984ff1f847bafbff1048fbaedfef3c641
eltjon.metko <eltjon.metko@cgm.com>
2023-09-20 15:57:38 +0200

    add search filter for namespaces

--- namaspaceModel.go
+++ namaspaceModel.go
@@ -3,16 +3,19 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/charmbracelet/bubbles/cursor"
 	"github.com/charmbracelet/bubbles/spinner"
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"strings"
 )
 
 type K8mNamespaceModel struct {
 	choices                  []string // items on the to-do list
-	cursor                   int      // which to-do list item our cursor is pointing at
-	selected                 string   // which to-do items are selected
+	filteredChoices          []string
+	cursor                   int    // which to-do list item our cursor is pointing at
+	selected                 string // which to-do items are selected
 	cluster                  kubernetesCluster
 	clusterConnectionSpinner spinner.Model
 	isConnected              bool
@@ -28,7 +31,8 @@ func initialModel() K8mNamespaceModel {
 
 	return K8mNamespaceModel{
 		// Our to-do list is a grocery list
-		choices: []string{},
+		choices:         []string{},
+		filteredChoices: []string{},
 
 		// A map which indicates which choices are selected. We're using
 		// the  map like a mathematical set. The keys refer to the indexes
@@ -47,12 +51,13 @@ func (m K8mNamespaceModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case errMsg:
 		m.error = msg
-		return m, tea.Quit
+		//return m, tea.Quit
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "ctrl+c", "esc":
 			return m, tea.Quit
-
+		case "ctrl+shift+f":
+			m.filter.Focus()
 		// The "up" and "k" keys move the cursor up
 		case "up", "k":
 			if m.cursor > 0 {
@@ -61,17 +66,30 @@ func (m K8mNamespaceModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 		// The "down" and "j" keys move the cursor down
 		case "down", "j":
-			if m.cursor < len(m.choices)-1 {
+			if m.cursor < len(m.filteredChoices)-1 {
 				m.cursor++
 			}
 
 		// The "enter" key and the spacebar (a literal space) toggle
 		// the selected state for the item that the cursor is pointing at.
 		case "enter", " ":
-			m.selected = m.choices[m.cursor]
+			m.selected = m.filteredChoices[m.cursor]
 			cmd := WakeUpReviewOperation.Command(m, m.selected)
-			return m, tea.Batch(cmd)
+			cmds = append(cmds, cmd)
+			//return m, tea.Batch(cmd)
 		}
+	case cursor.BlinkMsg:
+	default:
+		if m.filter.Value() != "" {
+			m.filteredChoices = []string{}
+			for _, namespace := range m.choices {
+				if strings.Contains(namespace, m.filter.Value()) {
+					m.filteredChoices = append(m.filteredChoices, namespace)
+				}
+			}
+			m.cursor = 0
+		}
+
 		//return m, nil
 	case clusterConnectedMsg:
 		m.isConnected = true
@@ -84,6 +102,7 @@ func (m K8mNamespaceModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		for _, item := range namespaces.Items {
 			m.choices = append(m.choices, item.ObjectMeta.Name)
 		}
+		m.filteredChoices = m.choices
 	case namespaceSelectedMsg:
 		tea.Batch()
 	}
@@ -107,10 +126,17 @@ func (m K8mNamespaceModel) View() string {
 		s += "Connecting to the cluster..."
 	} else {
 		s = m.filter.View()
-		s += "What should we buy at the market?\n\n"
+		s += "\nSelect a namespace\n\n"
 
 		// Iterate over our choices
-		for i, choice := range m.choices[:10] {
+
+		choicesLen := len(m.filteredChoices)
+		limit := choicesLen
+
+		if choicesLen > 10 {
+			limit = 10
+		}
+		for i, choice := range m.filteredChoices[:limit] {
 
 			// Is the cursor pointing at this choice?
 			cursor := " " // no cursor
