ba212041f1573adc5f2716ce9585b7c2c34ad662
Lorenzo Milicia <info@lorenzomilicia.dev>
2023-11-11 20:15:51 +0100

    Replace sort.Find because it wasn't working correctly

--- utils.go
+++ utils.go
@@ -2,16 +2,14 @@ package main
 
 import (
 	"github.com/charmbracelet/bubbles/list"
-	"sort"
-	"strings"
 )
 
 func FindItem(items []list.Item, search list.Item) int {
-	i, found := sort.Find(len(items), func(i int) int {
-		return strings.Compare(items[i].FilterValue(), search.FilterValue())
-	})
-	if !found {
-		return -1
+	var idx = -1
+	for i, item := range items {
+		if item.FilterValue() == search.FilterValue() {
+			idx = i
+		}
 	}
-	return i
+	return idx
 }
